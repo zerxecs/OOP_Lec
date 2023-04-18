b83e9790fa14fcc64c0df106f18cffbd8a91f6e1
zerxecs <118116892+zerxecs@users.noreply.github.com>
2023-04-18 22:32:08 +0800

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.mavenproject1;
 
 
-
-/**
- *
- * @author maala
- */
 public class Plant {
     String type;
     String size;
